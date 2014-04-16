d3f1fa099581c1644ef3694c8200127d1852d492
Justin Jia <justin@goodinc.com>
2014-04-16 01:31:46 -0700

    Refactor Api::EmailsController and update README.

--- app/controllers/api/emails_controller.rb
+++ app/controllers/api/emails_controller.rb
@@ -1,29 +1,25 @@
 class Api::EmailsController < ApplicationController
   def track
-    arr = params['_json']
-    if params[:_json].class.to_s == "Array"
-      arr.each do |i|
-        if i['category'] && i['category'].include?('messages')
-          if i['event'] == 'open'
-            user = User.find_by_email(i['email'])
-            user ||= Landlord.find_by_email(i['email'])
-            message_thread = MessageThread.find(i['thread_id'])
-            message_thread.mark_as_read(user) if user
-          end
-        end
-      end
-    else
-      unless params[:_json] == nil
-        if params[:_json]['category'].include? 'messages'
-          if params[:_json]['event'] == 'open'
-            user = User.find_by_email(params[:_json]['email'])
-            user ||= Landlord.find_by_email(params[:_json]['email'])
-            message_thread = MessageThread.find(params[:_json]['thread_id'])
-            message_thread.mark_as_read(user) if user
-          end
-        end
+    json = params['_json']
+    unless json.nil?
+      if json.class.to_s == "Array"
+        json.each{ |i| update_msg_thread i['email'], i['thread_id'] if response_is_open_event?(i) }
+      else
+        update_msg_thread(json['email'], json['thread_id']) if response_is_open_event?(json)
       end
     end
     render status: 200, json: true
+  end
+
+private
+
+  def update_msg_thread(email, thread_id)
+    user = User.find_by_email(email) || Landlord.find_by_email(email)
+    message_thread = MessageThread.find thread_id
+    message_thread.mark_as_read(user) if user && message_thread
+  end
+
+  def response_is_open_event?(json)
+    json['category'] && json['category'].include?('messages') && json['event'] == 'open'
   end
 end
